602b86d34bfe618c36646c54ce8e0abe6934d360
k3vinrich4rd <kevinrichardve@gmail.com>
2023-12-15 17:48:20 -0300

    Inserindo a criacao do verbo HTTP Post

--- src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -24,6 +21,11 @@ import java.util.List;
 public class AnimeController {
     private final DateUtil dateUtil;
     private final AnimeService animeService;
+
+    @PostMapping
+    public ResponseEntity<Anime> createAnime(@RequestBody Anime anime) {
+        return new ResponseEntity<>(animeService.createAnime(anime), HttpStatus.CREATED);
+    }
 
     @GetMapping
     public ResponseEntity<List<Anime>> listAll() {
--- src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
@@ -2,19 +2,22 @@ package academy.devdojo.springboot2essentials.service;
 
 import academy.devdojo.springboot2essentials.domain.Anime;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
     //private final AnimeRepository animeRepository (Futuro)
 
-    private final List<Anime> animes = List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Berserk"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Berserk")));
+    }
 
     public List<Anime> findById() {
         return animes;
@@ -25,5 +28,11 @@ public class AnimeService {
                 .stream()
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not Found"));
+    }
+
+    public Anime createAnime(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+        animes.add(anime);
+        return anime;
     }
 }

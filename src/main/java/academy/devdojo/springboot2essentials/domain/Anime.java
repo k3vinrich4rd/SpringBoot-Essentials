f2cf4db3e72c042f9ec1ae27fc6bf2f5c56b13b8
k3vinrich4rd <kevinrichardve@gmail.com>
2023-12-29 17:15:22 -0300

    Inserindo conexao com o banco de dados postgresql

--- src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
@@ -1,14 +1,21 @@
 package academy.devdojo.springboot2essentials.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.UUID;
+import javax.persistence.*;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+//@Table(name = "student_register") usado para definir um nome a tabela
+//Mas se esse nome não for definido a tabela terá o nome da classe
 public class Anime {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 

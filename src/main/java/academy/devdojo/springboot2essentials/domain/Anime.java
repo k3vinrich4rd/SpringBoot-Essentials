8ab0d01689ffb2d6ff4d4835c4deddfa1c1e4180
k3vinrich4rd <kevinrichardve@gmail.com>
2024-01-16 22:29:27 -0300

    implementando mudancas na camada domain

--- src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
@@ -23,5 +24,6 @@ public class Anime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     //@JsonProperty("casa") //caso o campo tivesse um nome diferente do que o informado
+    @NotBlank(message = "The anime name cannot be empty")
     private String name;
 }
